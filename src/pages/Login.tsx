b1519a26b8cf6859057513a79210400e787d0f3d
FernandoMos92 <fernandomosp@gmail.com>
2022-07-01 20:48:35 -0300

    :sparkles: Add lib for icons and import icon

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Login.scss';
+import { BiUser } from 'react-icons/bi';
 
 export default function Login() {
   return (
@@ -7,10 +8,16 @@ export default function Login() {
       <h1>Login</h1>
       <h2>To Do List</h2>
       <form>
-        <input type="text" />
-        <input type="password" />
+        <input 
+          type="text" 
+          placeholder='email'
+        />
+        <input 
+          type="password" 
+          placeholder='password'
+        />
       </form>
-      <div>
+      <div className='container-input-sign'>
         <button>Sign in</button>
         <span>Register Now</span>
       </div>
